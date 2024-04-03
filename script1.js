0338e068bc7b229e46ac8cba6618565d8d9266b9
kunal patel <140698610+kunalpatel196@users.noreply.github.com>
2024-04-03 16:32:46 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,338 @@
+
+  
+  function play(){
+    
+            
+            var src = "video/A Quiet Place_ Day One _ Official Trailer (2024 Movie) - Lupita Nyong'o, Joseph Quinn.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play2(){
+    
+            
+            var src = "video/Avatar_ The Last Airbender _ Official Trailer _ Netflix.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play3(){
+    
+            
+            var src = "video/Borderlands (2024) Official Trailer - Cate Blanchett, Kevin Hart, Jack Black.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play4(){
+    
+            
+            var src = "video/Damsel _ Official Trailer _ Netflix.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play5(){
+    
+            
+            var src = "video/DEADSHOT — Official AI Trailer (2024) _ Will Smith Action Movie.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play6(){
+    
+            
+            var src = "video/Float     Official Trailer  _ 2024  Movies Trailer.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play7(){
+    
+            
+            var src = "video/Avatar_ The Last Airbender _ Official Trailer _ Netflix.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play8(){
+    
+            
+            var src = "video/Logan 2 - First Trailer _ Hugh Jackman, Dafne Keen.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play9(){
+    
+            
+            var src = "video/Avatar_ The Last Airbender _ Official Trailer _ Netflix.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play10(){
+    
+            
+            var src = "video/MOANA Live Action - Official Trailer (2024) Zendaya, Dwayne Johnson _ Disney+.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play11(){
+    
+            
+            var src = "video/The Gentlemen _ A Guy Ritchie Series Official Trailer _ Netflix.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
+  function play12(){
+    
+            
+            var src = "video/Godzilla x Kong_ The New Empire _ Official Trailer 2.mp4";
+    
+            
+            var video = document.createElement("video");
+    
+          
+            video.src = src;
+    
+            
+            video.controls = true;
+            video.autoplay = true; 
+    
+            
+    
+            
+            function openInNewTab(url) {
+                var win = window.open(url, '_blank');
+                win.focus();
+            }
+    
+            
+            openInNewTab(src);
+    
+
+  }
